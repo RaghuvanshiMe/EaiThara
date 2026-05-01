18ce65b3bcbade7ad3a7829ca994a280b83106b7
Mihir Vikram Singh <mihirsingh23@hotmail.com>
2026-05-01 16:34:37 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://eaithara.onrender.com',
         changeOrigin: true
       }
     }
